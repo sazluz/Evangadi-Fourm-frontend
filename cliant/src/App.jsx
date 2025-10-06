00558c585caeb89bfc82ce28c97874f94b2060b6
Dese-1216 <desalegnmengsitu@gmail>
2025-10-06 02:36:18 -0700

    initial frontend commit

---
+++ cliant/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
