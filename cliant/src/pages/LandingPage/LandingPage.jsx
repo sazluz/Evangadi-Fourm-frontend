79bb7eb1f59d95255b6220ccde37c038926b089b
Salahadin Admasu <104188060+sazluz@users.noreply.github.com>
2025-10-26 12:35:00 +0300

    Refactor background div and clean up comments

--- cliant/src/pages/LandingPage/LandingPage.jsx
+++ cliant/src/pages/LandingPage/LandingPage.jsx
@@ -5,14 +5,10 @@ import { Link } from "react-router-dom";
 function LandingPage() {
   return (
     <div>
-      {/* Use inline style to set the background image from the public folder */}
-      <div
-        className={classes.background}
-        style={{ backgroundImage: `url("/Landingpage1.jpg")` }}
-      >
+      <div className={classes.background}>
         <div className={classes.textDiv}>
           <h2>
-            Bypass the <br /> Industrial, <br /> Dive into the Digital!
+            Bypass the <br/> Industrial, <br /> Dive into the Digital!
           </h2>
           <p>
             Before us is a golden opportunity, demanding us to take a bold step
