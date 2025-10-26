e5a99b2b6a37a3324d9b70b690a8a3060a68c01d
Salahadin Admasu <104188060+sazluz@users.noreply.github.com>
2025-10-26 12:28:54 +0300

    Refactor LandingPage to use public folder image

--- cliant/src/pages/LandingPage/LandingPage.jsx
+++ cliant/src/pages/LandingPage/LandingPage.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import classes from "./LandingPage.module.css";
 import { Link } from "react-router-dom";
-import LandingImage from "../../assets/images/Landingpage1.jpg"; // adjust path if needed
 
 function LandingPage() {
   return (
-    <div
-      className={classes.background}
-      style={{
-        backgroundImage: `url(${LandingImage})`,
-      }}
-    >
-      <div className={classes.textDiv}>
-        <h2>
-          Bypass the <br /> Industrial, <br /> Dive into the Digital!
-        </h2>
-        <p>
-          Before us is a golden opportunity, demanding us to take a bold step
-          forward and join the new digital era.
-        </p>
-        <div className={classes.linkDiv}>
-          <Link className={classes.createAccount} to="/register">
-            Create Account
-          </Link>
-          <Link className={classes.signIn} to="/login">
-            Sign In
-          </Link>
+    <div>
+      {/* Use inline style to set the background image from the public folder */}
+      <div
+        className={classes.background}
+        style={{ backgroundImage: `url("/Landingpage1.jpg")` }}
+      >
+        <div className={classes.textDiv}>
+          <h2>
+            Bypass the <br /> Industrial, <br /> Dive into the Digital!
+          </h2>
+          <p>
+            Before us is a golden opportunity, demanding us to take a bold step
+            forward and join the new digital era.
+          </p>
+          <div className={classes.linkDiv}>
+            <Link className={classes.createAccount} to="/register">
+              Create Account
+            </Link>
+            <Link className={classes.signIn} to="/login">
+              Sign In
+            </Link>
+          </div>
         </div>
       </div>
     </div>
@@ -33,4 +33,3 @@ function LandingPage() {
 }
 
 export default LandingPage;
-
